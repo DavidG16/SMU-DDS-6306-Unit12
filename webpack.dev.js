49542b8c047fdcfd38abc29f320b342e4591a1e6
David Grijalva <dgrijalva@indeed.com>
2020-11-05 12:56:41 -0600

    new changes

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 
 const path = require('path')
 const common = require("./webpack.common");
-const merge = require("webpack-merge");
+const {merge} = require("webpack-merge");
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = merge(common, {
