1a4cebf2ab7fe64a1ec5955b5c7ed13d479f5542
David Grijalva <dgrijalva@indeed.com>
2020-11-04 18:46:44 -0600

    Add webpack and main files

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+
+module.exports = {
+  entry: {
+    main: "./src/client/index.js",
+  },
+  module: {
+    rules: [
+        {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+        },    
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }}}]},
+  plugins: [
+     new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
+  }),
+]
+};
---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+
+const path = require('path')
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        })
+    ]
+})
